8b66c401242cf010a347785a76a55991f02e1668
Ian Moses <imoses2@hotmail.com>
2023-12-15 21:57:47 -0700

    the model only works if it was trained on a cpu

--- server/public/model.js
+++ server/public/model.js
@@ -2,12 +2,16 @@ let model;
 let canvas = document.getElementById('mapImage');
 let satelliteImageElement = document.getElementById('satelliteImage')
 const offset = tf.scalar(127.5);
-const one = tf.scalar(1);
+const one = tf.scalar(1.0);
 
 
 let loadModel = async () => {
+	//tf.setBackend('cpu');
 	model = await tf.loadLayersModel('./2 rounds cpu/model.json');
+	//model = await tf.loadLayersModel('./2 rounds/model.json');
+	//model = await tf.loadLayersModel('./2 rounds t4/model.json');
 	console.log('done loading');
+	console.log(tf.getBackend());
 }
 
 let preProcess = () => {
@@ -26,13 +30,17 @@ let preProcess = () => {
 	return imageTensor;
 }
 
-let predict = () => {
+let predict = async () => {
 	// preprocess
-	const imageTensor = preProcess();
+	const imageTensor = await preProcess();
 	
 	// make the prediction
-	let output = model.predict(imageTensor);
+	let output = model.predict(imageTensor)
+	output.data()
+		.then(d => console.log(d));
+	
 	output = tf.squeeze(output);
+	//console.log(output);
 	
 	// convert it back to integers
 	output = output.add(one).mul(offset).cast('int32');
